fe444e008fe440ef372188e1ea45282e80bde00a
ivzaislu <johann0789@googlemail.com>
2026-09-03 06:18:38 +0200

    Add Krasview models

---
+++ Modules/OnlineRUS/Krasview/Model.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Krasview;
+
+public class SearchItem
+{
+    public string url { get; set; }
+    public string host { get; set; }
+    public string kind { get; set; }
+    public string slug { get; set; }
+    public string en { get; set; }
+    public int year { get; set; }
+}
+
+public class VideoItem
+{
+    public string id { get; set; }
+    public string href { get; set; }
+    public string slug { get; set; }
+    public int s { get; set; }
+    public int e { get; set; }
+}
+
+public class SeasonItem
+{
+    public int id { get; set; }
+}
+
+public class VideoConfig
+{
+    public string url { get; set; }
+    public Dictionary<string, JsonElement> audio_info { get; set; }
+}
