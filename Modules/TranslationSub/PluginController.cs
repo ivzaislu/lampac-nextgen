63582b3384db11d2824692696d76bd2ea1c02280
ivzaislu <johann0789@googlemail.com>
2026-09-07 13:16:49 +0200

    Load redesigned TranslationSub UI

--- Modules/TranslationSub/PluginController.cs
+++ Modules/TranslationSub/PluginController.cs
@@ -24,6 +24,7 @@ public class TranslationSubPluginController : BaseController
 
         AppendScript(ref script, "translationsub-page.js");
         AppendScript(ref script, "translationsub-watch.js");
+        AppendScript(ref script, "translationsub-ui.js");
 
         Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
         return Content(script, "application/javascript; charset=utf-8");
