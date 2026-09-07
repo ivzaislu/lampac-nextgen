ca42e5569b3f5e0586a5b1fd9a0e3ecfbdc31a36
ivzaislu <johann0789@googlemail.com>
2026-09-08 01:29:03 +0200

    feat: load TranslationSub settings v2

--- Modules/TranslationSub/PluginController.cs
+++ Modules/TranslationSub/PluginController.cs
@@ -30,6 +30,7 @@ public class TranslationSubPluginController : BaseController
         AppendScript(ref script, "translationsub-notice.js");
         AppendScript(ref script, "translationsub-mobile.js");
         AppendScript(ref script, "translationsub-polish.js");
+        AppendScript(ref script, "translationsub-settings-v2.js");
 
         Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
         return Content(script, "application/javascript; charset=utf-8");
