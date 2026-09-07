1619135a83bd17f50aa296e8f2b15ff64670642d
ivzaislu <johann0789@googlemail.com>
2026-09-08 00:32:49 +0200

    fix: make subscription store mutations atomic

--- Modules/TranslationSub/Services/SubscriptionStore.cs
+++ Modules/TranslationSub/Services/SubscriptionStore.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using TranslationSub.Models;
@@ -10,30 +11,73 @@ public static class SubscriptionStore
     static readonly object locker = new();
     static string path => "database/translationsub/subscriptions.json";
 
+    static List<TranslationSubscription> LoadUnsafe()
+    {
+        if (!File.Exists(path))
+            return new List<TranslationSubscription>();
+
+        try
+        {
+            return JsonConvert.DeserializeObject<List<TranslationSubscription>>(File.ReadAllText(path))
+                ?? new List<TranslationSubscription>();
+        }
+        catch
+        {
+            return new List<TranslationSubscription>();
+        }
+    }
+
+    static void SaveUnsafe(List<TranslationSubscription> list)
+    {
+        string dir = Path.GetDirectoryName(path);
+        if (!string.IsNullOrWhiteSpace(dir))
+            Directory.CreateDirectory(dir);
+
+        string json = JsonConvert.SerializeObject(list ?? new List<TranslationSubscription>(), Formatting.Indented);
+        string temp = path + ".tmp";
+
+        // Пишем сначала во временный файл, чтобы subscriptions.json не остался
+        // наполовину записанным при аварийном завершении процесса.
+        File.WriteAllText(temp, json);
+        File.Move(temp, path, true);
+    }
+
     public static List<TranslationSubscription> Load()
     {
         lock (locker)
-        {
-            if (!File.Exists(path))
-                return new List<TranslationSubscription>();
-
-            try
-            {
-                return JsonConvert.DeserializeObject<List<TranslationSubscription>>(File.ReadAllText(path)) ?? new List<TranslationSubscription>();
-            }
-            catch
-            {
-                return new List<TranslationSubscription>();
-            }
-        }
+            return LoadUnsafe();
     }
 
     public static void Save(List<TranslationSubscription> list)
     {
         lock (locker)
+            SaveUnsafe(list);
+    }
+
+    public static void Mutate(Action<List<TranslationSubscription>> action)
+    {
+        if (action == null)
+            return;
+
+        lock (locker)
         {
-            Directory.CreateDirectory(Path.GetDirectoryName(path));
-            File.WriteAllText(path, JsonConvert.SerializeObject(list, Formatting.Indented));
+            var list = LoadUnsafe();
+            action(list);
+            SaveUnsafe(list);
+        }
+    }
+
+    public static T Mutate<T>(Func<List<TranslationSubscription>, T> action)
+    {
+        if (action == null)
+            return default;
+
+        lock (locker)
+        {
+            var list = LoadUnsafe();
+            T result = action(list);
+            SaveUnsafe(list);
+            return result;
         }
     }
 }
