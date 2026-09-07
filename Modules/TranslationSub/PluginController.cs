6b19d39310f2d71a71a01d95995de91670ac49a9
ivzaislu <johann0789@googlemail.com>
2026-09-07 12:56:11 +0200

    Load TranslationSub watch progress plugin

--- Modules/TranslationSub/PluginController.cs
+++ Modules/TranslationSub/PluginController.cs
@@ -21,12 +21,18 @@ public class TranslationSubPluginController : BaseController
             return NotFound();
 
         string script = System.IO.File.ReadAllText(path);
-        string pagePath = Path.Combine(ModInit.modpath, "translationsub-page.js");
 
-        if (System.IO.File.Exists(pagePath))
-            script += "\n;\n" + System.IO.File.ReadAllText(pagePath);
+        AppendScript(ref script, "translationsub-page.js");
+        AppendScript(ref script, "translationsub-watch.js");
 
         Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
         return Content(script, "application/javascript; charset=utf-8");
+    }
+
+    static void AppendScript(ref string script, string filename)
+    {
+        string file = Path.Combine(ModInit.modpath, filename);
+        if (System.IO.File.Exists(file))
+            script += "\n;\n" + System.IO.File.ReadAllText(file);
     }
 }
