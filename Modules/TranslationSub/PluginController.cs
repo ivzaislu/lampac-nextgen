25b7d04abe4e1d9332c44e0c40f0ed89c86958e1
ivzaislu <johann0789@googlemail.com>
2026-09-10 23:19:45 +0200

    TranslationSub: load realtime client after badge state

--- Modules/TranslationSub/PluginController.cs
+++ Modules/TranslationSub/PluginController.cs
@@ -34,6 +34,7 @@ public class TranslationSubPluginController : BaseController
         AppendScript(ref script, "translationsub-card-flow.js");
         AppendScript(ref script, "translationsub-layout-v3.js");
         AppendScript(ref script, "translationsub-badge-state.js");
+        AppendScript(ref script, "translationsub-realtime.js");
         AppendScript(ref script, "translationsub-navigation.js");
         AppendScript(ref script, "translationsub-bell-theme.js");
 
