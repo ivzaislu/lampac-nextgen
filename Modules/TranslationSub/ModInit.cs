e5bc22101a7d5891ce1549f33c89fad06fcc03ab
ivzaislu <johann0789@googlemail.com>
2026-09-10 23:18:51 +0200

    TranslationSub: publish TimeCode changes through Lampac NWS

--- Modules/TranslationSub/ModInit.cs
+++ Modules/TranslationSub/ModInit.cs
@@ -1,10 +1,15 @@
+using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using Shared.Models.Base;
 using Shared.Models.Events;
 using Shared.Models.Module;
 using Shared.Models.Module.Interfaces;
 using Shared.Services;
+using System;
 using System.IO;
 using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using TranslationSub.Services;
 
 namespace TranslationSub;
@@ -22,12 +27,18 @@ public class ModInit : IModuleLoaded
         updateConf();
         EventListener.UpdateInitFile += updateConf;
         EventListener.AppReplace += appReplace;
+        EventListener.NwsMessage += nwsMessage;
+        EventListener.NwsDisconnected += nwsDisconnected;
+        EventListener.Middleware += middleware;
     }
 
     public void Dispose()
     {
         EventListener.UpdateInitFile -= updateConf;
         EventListener.AppReplace -= appReplace;
+        EventListener.NwsMessage -= nwsMessage;
+        EventListener.NwsDisconnected -= nwsDisconnected;
+        EventListener.Middleware -= middleware;
         TranslationSubscriptionService.Stop();
     }
 
@@ -39,6 +50,60 @@ public class ModInit : IModuleLoaded
             TranslationSubscriptionService.Start();
         else
             TranslationSubscriptionService.Stop();
+    }
+
+    static void nwsMessage(EventNwsMessage e)
+    {
+        if (conf?.enable != true || e == null
+            || !string.Equals(e.method, "TranslationSubRegister", StringComparison.Ordinal)
+            || e.args.ValueKind != JsonValueKind.Array
+            || e.args.GetArrayLength() < 1)
+            return;
+
+        try
+        {
+            string uid = e.args[0].ToString();
+            string profileId = e.args.GetArrayLength() > 1 ? e.args[1].ToString() : "0";
+            TranslationSubRealtimeService.Register(e.connectionId, uid, profileId);
+        }
+        catch { }
+    }
+
+    static void nwsDisconnected(EventNwsDisconnected e)
+    {
+        if (e != null)
+            TranslationSubRealtimeService.Unregister(e.connectionId);
+    }
+
+    static bool middleware(bool first, EventMiddleware e)
+    {
+        if (first || conf?.enable != true || e?.httpContext == null)
+            return true;
+
+        var context = e.httpContext;
+        var request = context.Request;
+        if (!HttpMethods.IsPost(request.Method)
+            || !string.Equals(request.Path.Value, "/timecode/add", StringComparison.OrdinalIgnoreCase))
+            return true;
+
+        var requestInfo = context.Features.Get<RequestModel>();
+        string uid = requestInfo?.user_uid?.Trim();
+        if (string.IsNullOrWhiteSpace(uid))
+            return true;
+
+        string profileId = request.Query.TryGetValue("profile_id", out var profileQuery)
+            ? profileQuery.ToString()
+            : "0";
+
+        context.Response.OnCompleted(() =>
+        {
+            if (context.Response.StatusCode >= 200 && context.Response.StatusCode < 300)
+                return TranslationSubRealtimeService.PublishProfile(uid, profileId, "timecode");
+
+            return Task.CompletedTask;
+        });
+
+        return true;
     }
 
     static StringBuilder appReplace(string type, EventAppReplace e)
