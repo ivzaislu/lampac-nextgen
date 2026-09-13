727a47311003380c989900b242cec5081247569f
ivzaislu <johann0789@googlemail.com>
2026-09-13 19:59:15 +0200

    fix(FlixCDN): restore serial player payload fields

--- Modules/OnlineRUS/FlixCDN/Model.cs
+++ Modules/OnlineRUS/FlixCDN/Model.cs
@@ -44,9 +44,19 @@ public class PlayerPayload
 
     public bool is_serial { get; set; }
 
+    public string type { get; set; }
+
     public int translate { get; set; }
 
     public string translateTitle { get; set; }
+
+    public short? season { get; set; }
+
+    public int[] episodes { get; set; }
+
+    public Dictionary<string, int> seasons { get; set; }
+
+    public Dictionary<string, int[]> seasons_episodes { get; set; }
 
     public List<PlayerTranslation> translations { get; set; }
 }
@@ -56,4 +66,6 @@ public class PlayerTranslation
     public int id { get; set; }
 
     public string title { get; set; }
+
+    public int episodes_qty { get; set; }
 }
