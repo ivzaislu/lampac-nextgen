e03fe48d85f9c27206ce38aba80036243c4d9eca
ivzaislu <johann0789@googlemail.com>
2026-09-25 03:39:07 +0200

    vkseries: preserve VK playlist positions

--- Modules/OnlineRUS/VkSeries/Model.cs
+++ Modules/OnlineRUS/VkSeries/Model.cs
@@ -115,6 +115,7 @@ public class Video
     public string description { get; set; }
     public long duration { get; set; }
     public long? views { get; set; }
+    public int playlist_position { get; set; }
     public string owner_name { get; set; }
     public ShortVideoInfo short_video_info { get; set; }
     public VideoFiles files { get; set; }
